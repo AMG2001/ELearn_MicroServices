48229aa10626e1313e47c674f2ca3f411403ee63
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-05-10 10:48:15 +0300

    Eureka Server Added.

---
+++ registry-server/registry-server/src/main/java/edu/elean/registryserver/RegistryServerApplication.java
@@ -0,0 +1,13 @@
+package edu.elean.registryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+
+}
---
+++ registry-server/registry-server/src/test/java/edu/elean/registryserver/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.elean.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
